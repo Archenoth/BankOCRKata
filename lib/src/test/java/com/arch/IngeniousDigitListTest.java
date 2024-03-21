65fdeeb986f2826bc41ad7d0e0dedca8e9f1bd99
Archenoth <archenoth@gmail.com>
2024-03-21 16:21:49 -0600

    Cleaned up (And renamed!) IngeniousDigitList tests

---
+++ lib/src/test/java/com/arch/IngeniousDigitListTest.java
@@ -0,0 +1,77 @@
+package com.arch;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class IngeniousDigitListTest {
+    private static final String ZEROS =
+        " _  _  _  _  _  _  _  _  _ \n" +
+        "| || || || || || || || || |\n" +
+        "|_||_||_||_||_||_||_||_||_|\n";
+
+    private static final String ONES =
+        "                           \n" +
+        "  |  |  |  |  |  |  |  |  |\n" +
+        "  |  |  |  |  |  |  |  |  |\n";
+
+    private static final String TWOS =
+        " _  _  _  _  _  _  _  _  _ \n" +
+        " _| _| _| _| _| _| _| _| _|\n" +
+        "|_ |_ |_ |_ |_ |_ |_ |_ |_ \n";
+
+    private static final String THREES =
+        " _  _  _  _  _  _  _  _  _ \n" +
+        " _| _| _| _| _| _| _| _| _|\n" +
+        " _| _| _| _| _| _| _| _| _|\n";
+
+    private static final String FOURS =
+        "                           \n" +
+        "|_||_||_||_||_||_||_||_||_|\n" +
+        "  |  |  |  |  |  |  |  |  |\n";
+
+    private static final String FIVES =
+        " _  _  _  _  _  _  _  _  _ \n" +
+        "|_ |_ |_ |_ |_ |_ |_ |_ |_ \n" +
+        " _| _| _| _| _| _| _| _| _|\n";
+
+    private static final String SIXES =
+        " _  _  _  _  _  _  _  _  _ \n" +
+        "|_ |_ |_ |_ |_ |_ |_ |_ |_ \n" +
+        "|_||_||_||_||_||_||_||_||_|\n";
+
+    private static final String SEVENS =
+        " _  _  _  _  _  _  _  _  _ \n" +
+        "  |  |  |  |  |  |  |  |  |\n" +
+        "  |  |  |  |  |  |  |  |  |\n";
+
+    private static final String EIGHTS =
+        " _  _  _  _  _  _  _  _  _ \n" +
+        "|_||_||_||_||_||_||_||_||_|\n" +
+        "|_||_||_||_||_||_||_||_||_|\n";
+
+    private static final String NINES =
+        " _  _  _  _  _  _  _  _  _ \n" +
+        "|_||_||_||_||_||_||_||_||_|\n" +
+        " _| _| _| _| _| _| _| _| _|\n";
+
+
+    private static final String COUNT =
+        "    _  _     _  _  _  _  _ \n" +
+        "  | _| _||_||_ |_   ||_||_|\n" +
+        "  ||_  _|  | _||_|  ||_| _|\n";
+
+    @Test void useCase1() {
+        assertEquals("000000000", new IngeniousDigitList(ZEROS).toString());
+        assertEquals("111111111", new IngeniousDigitList(ONES).toString());
+        assertEquals("222222222", new IngeniousDigitList(TWOS).toString());
+        assertEquals("333333333", new IngeniousDigitList(THREES).toString());
+        assertEquals("444444444", new IngeniousDigitList(FOURS).toString());
+        assertEquals("555555555", new IngeniousDigitList(FIVES).toString());
+        assertEquals("666666666", new IngeniousDigitList(SIXES).toString());
+        assertEquals("777777777", new IngeniousDigitList(SEVENS).toString());
+        assertEquals("888888888", new IngeniousDigitList(EIGHTS).toString());
+        assertEquals("999999999", new IngeniousDigitList(NINES).toString());
+        assertEquals("123456789", new IngeniousDigitList(COUNT).toString());
+    }
+}
