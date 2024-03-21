1320aff41967b42644a65a8ba521eec63a868804
Archenoth <archenoth@gmail.com>
2024-03-21 15:40:03 -0600

    Test cases pass for story 1

---
+++ lib/src/main/java/com/arch/IngeniousDigit.java
@@ -0,0 +1,69 @@
+package com.arch;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IngeniousDigit {
+    /**
+     * The 3x3 String representation of this digit
+     */
+    private final String _digit;
+
+    private static List<String> DIGIT_STRINGS = Arrays.asList(
+        " _ \n" +
+        "| |\n" +
+        "|_|\n",
+
+        "   \n" +
+        "  |\n" +
+        "  |\n",
+
+        " _ \n" +
+        " _|\n" +
+        "|_ \n",
+
+        " _ \n" +
+        " _|\n" +
+        " _|\n",
+
+        "   \n" +
+        "|_|\n" +
+        "  |\n",
+
+        " _ \n" +
+        "|_ \n" +
+        " _|\n",
+
+        " _ \n" +
+        "|_ \n" +
+        "|_|\n",
+
+        " _ \n" +
+        "  |\n" +
+        "  |\n",
+
+        " _ \n" +
+        "|_|\n" +
+        "|_|\n",
+
+        " _ \n" +
+        "|_|\n" +
+        " _|\n"
+    );
+
+    public IngeniousDigit(String digit){
+        _digit = digit;
+    }
+
+    public IngeniousDigit(int digit){
+        _digit = DIGIT_STRINGS.get(digit);
+    }
+
+    public int toInt(){
+        return toInt(_digit);
+    }
+
+    public static int toInt(String digit){
+        return DIGIT_STRINGS.indexOf(digit);
+    }
+}
